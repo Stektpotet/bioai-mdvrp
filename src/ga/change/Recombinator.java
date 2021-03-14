d70bceb6bff12fc00949e3ff308150aeeafd5452
Halvor Smedås <stektpotet@gmail.com>
2021-03-14 03:14:27 +0100

    Adding recombinateAll structure to Recombinator

--- src/ga/change/Recombinator.java
+++ src/ga/change/Recombinator.java
@@ -1,6 +1,10 @@
 package ga.change;
 
-public interface Recombinator<C> {
-    public C[] recombine(C[] parents);
-    C[] crossover(C mama, C papa);
+import ga.data.Chromosome;
+
+import java.util.List;
+
+public interface Recombinator<C extends Chromosome> {
+    public List<C> recombine(List<C> parents);
+    List<C> crossover(C mum, C dad);
 }
